61acb77648514ea9cda33cebb6b06f71af6366a1
shokoro3434 <nonamennm03@gmail.com>
2015-04-06 11:45:35 +0900

    rm

--- src/jp/ac/nct/math/SingleRegressionAnalysis.java
+++
@@ -1,7 +0,0 @@
-package jp.ac.nct.math;
-
-public interface SingleRegressionAnalysis {
-	public abstract double getA ();
-	public abstract double getB ();
-	public abstract String getRegressionEquation ();
-}
