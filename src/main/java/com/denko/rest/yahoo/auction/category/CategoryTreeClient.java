b84ce1d5aa7b2835f89314a40c3e099a35bf6b68
sokoro <nonamennm03@gmail.com>
2015-08-12 15:03:49 +0900

    fixed #15

--- src/main/java/com/denko/rest/yahoo/auction/category/CategoryTreeClient.java
+++ src/main/java/com/denko/rest/yahoo/auction/category/CategoryTreeClient.java
@@ -1,5 +1,7 @@
 package com.denko.rest.yahoo.auction.category;
 
+import java.io.IOException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,11 @@ import org.springframework.boot.orm.jpa.EntityScan;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 import com.denko.model.Category;
@@ -35,39 +41,62 @@ public class CategoryTreeClient {
 //        SpringApplication.run(CategoryTreeClient.class);
     } 
 
-    private void register(Integer categoryId){
-    	StringBuffer sb = new StringBuffer();
-    	sb.append("http://auctions.yahooapis.jp/AuctionWebService/V2/categoryTree");
-    	sb.append("?");
-    	sb.append("appid=");
-    	sb.append(System.getProperty("appid"));
-    	sb.append("&");
-    	sb.append("adf=1");
-    	sb.append("&");
-    	sb.append("category=");
-    	sb.append(categoryId);
-    	RestTemplate restTemplate = new RestTemplate();
-        ResultSet resultSet = restTemplate.getForObject(sb.toString(), ResultSet.class);
-        Result result = resultSet.getResult();
-        if (!result.getIsLeaf()){
-        	for (ChildCategory child : result.getChildCategory()){
-	        	Integer yCategoryId = child.getCategoryId();
-	        	Boolean isLeaf = child.getIsLeaf();
-	        	if (isLeaf){
-		        	yahooCategoryService.deleteByYCategoryId(yCategoryId);
-		        	Category category = new Category();
-		        	category.setCategoryName(child.getCategoryName());
-		        	category.setCategoryPath(child.getCategoryPath());
-		        	category.setNumOfAuctions(child.getNumOfAuctions());
-		        	category.setParentCategoryId(child.getParentCategoryId());
-		        	category.setYCategoryId(yCategoryId);
-		        	yahooCategoryService.save(category);
-	        	}
-	        	else{
-	        		register(yCategoryId);
-	        	}
+    private void register(Integer categoryId) throws InterruptedException{
+    	try{
+	    	StringBuffer sb = new StringBuffer();
+	    	sb.append("http://auctions.yahooapis.jp/AuctionWebService/V2/categoryTree");
+	    	sb.append("?");
+	    	sb.append("appid=");
+	    	sb.append(System.getProperty("appid"));
+	    	sb.append("&");
+	    	sb.append("adf=1");
+	    	sb.append("&");
+	    	sb.append("category=");
+	    	sb.append(categoryId);
+	    	RestTemplate restTemplate = new RestTemplate();
+	//        restTemplate.setErrorHandler(new ResponseErrorHandler(){
+	//
+	//			@Override
+	//			public boolean hasError(ClientHttpResponse response) throws IOException {
+	//				// TODO Auto-generated method stub
+	//				Thread.sleep(10000);
+	//				return false;
+	//			}
+	//
+	//			@Override
+	//			public void handleError(ClientHttpResponse response) throws IOException {
+	//				// TODO Auto-generated method stub
+	//				
+	//			}
+	//        });
+	        ResultSet resultSet = restTemplate.getForObject(sb.toString(), ResultSet.class);
+	        Result result = resultSet.getResult();
+	        if (!result.getIsLeaf()){
+	        	for (ChildCategory child : result.getChildCategory()){
+		        	Integer yCategoryId = child.getCategoryId();
+		        	Boolean isLeaf = child.getIsLeaf();
+					Thread.sleep(500);
+		        	if (isLeaf){
+			        	yahooCategoryService.deleteByYCategoryId(yCategoryId);
+			        	Category category = new Category();
+			        	category.setCategoryName(child.getCategoryName());
+			        	category.setCategoryPath(child.getCategoryPath());
+			        	category.setNumOfAuctions(child.getNumOfAuctions());
+			        	category.setParentCategoryId(child.getParentCategoryId());
+			        	category.setYCategoryId(yCategoryId);
+			        	yahooCategoryService.save(category);
+		        	}
+		        	else{
+		        		register(yCategoryId);
+		        	}
+		        }
 	        }
-        }
+    	}
+    	catch(HttpServerErrorException e){
+    		e.printStackTrace();
+			Thread.sleep(10000);
+    		register(categoryId);
+    	}
     }
     
     public void invoke() throws Exception {
