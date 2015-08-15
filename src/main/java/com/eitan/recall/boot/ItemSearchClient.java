187875b425a3fe796487228aa6a5679cec3406d9
sokoro <nonamennm03@gmail.com>
2015-08-15 09:25:42 +0900

    a

--- src/main/java/com/eitan/recall/boot/ItemSearchClient.java
+++ src/main/java/com/eitan/recall/boot/ItemSearchClient.java
@@ -1,55 +1,18 @@
 package com.eitan.recall.boot;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.io.StringReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
-import java.util.TimeZone;
-
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLSession;
-import javax.xml.bind.JAXB;
-
-import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.DefaultResponseErrorHandler;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.ResponseErrorHandler;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.eitan.recall.model.Category;
-import com.eitan.recall.rest.amazon.xsd.ItemSearchResponse;
-import com.eitan.recall.rest.yahoo.auction.category.ChildCategory;
-import com.eitan.recall.rest.yahoo.auction.category.Result;
-import com.eitan.recall.rest.yahoo.auction.category.ResultSet;
-import com.eitan.recall.service.RecallService;
-import com.eitan.recall.service.YahooCategoryService;
 
+@EnableScheduling
 @SpringBootApplication
-@ComponentScan(basePackages={"com.eitan.recall.rest.amazon.itemsearch"})
-//@ComponentScan(basePackages = "com.eitan.recall.service")
+@ComponentScan(basePackages={"com.eitan.recall.rest.amazon.itemsearch","com.eitan.recall.service"})
 @EnableJpaRepositories(basePackages = "com.eitan.recall.repository")
 @EntityScan(basePackages = "com.eitan.recall.model")
 public class ItemSearchClient {
