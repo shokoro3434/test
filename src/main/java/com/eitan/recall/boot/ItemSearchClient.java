7414eb97622cd82ffc3b9c32fa8385c3229bbf58
sokoro <nonamennm03@gmail.com>
2015-08-16 06:49:39 +0900

    a

--- src/main/java/com/eitan/recall/boot/ItemSearchClient.java
+++ src/main/java/com/eitan/recall/boot/ItemSearchClient.java
@@ -1,7 +1,5 @@
 package com.eitan.recall.boot;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.orm.jpa.EntityScan;
@@ -9,16 +7,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
 @EnableScheduling
 @SpringBootApplication
 @ComponentScan(basePackages={"com.eitan.recall.rest.amazon.itemsearch","com.eitan.recall.service"})
 @EnableJpaRepositories(basePackages = "com.eitan.recall.repository")
 @EntityScan(basePackages = "com.eitan.recall.model")
 public class ItemSearchClient {
-	// @Autowired
-	// private YahooCategoryService yahooCategoryService;
-	private static final Logger log = LoggerFactory.getLogger(ItemSearchClient.class);
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ItemSearchClient.class);
     }
