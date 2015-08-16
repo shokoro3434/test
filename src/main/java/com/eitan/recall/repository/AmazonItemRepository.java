1c5c02c5c46dde56f288126539ec36c48437bb3b
sokoro <nonamennm03@gmail.com>
2015-08-16 11:30:41 +0900

    fixed

--- src/main/java/com/eitan/recall/repository/AmazonItemRepository.java
+++ src/main/java/com/eitan/recall/repository/AmazonItemRepository.java
@@ -1,7 +1,5 @@
 package com.eitan.recall.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.eitan.recall.model.AmazonItem;
