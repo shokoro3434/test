e492804af6402c4ee55bdcb7d6e63d0ead197e28
takahiro <takahiro@ThinkPadX201>
2015-08-31 04:56:37 +0900

    a

---
+++ src/main/java/com/eitax/recall/amazon/dao/AmazonItemService.java
@@ -0,0 +1,22 @@
+package com.eitax.recall.amazon.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.eitan.recall.model.AmazonItem;
+import com.eitan.recall.repository.AmazonItemRepository;
+
+@Component
+public class AmazonItemService {
+    @Autowired
+    private AmazonItemRepository amazonItemRepository;
+    
+	public AmazonItem save (AmazonItem ai){
+		return amazonItemRepository.save(ai);
+	}
+  
+}
